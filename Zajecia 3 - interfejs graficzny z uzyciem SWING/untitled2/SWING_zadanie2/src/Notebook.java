c86e250743cfb9607a91877d8586a6e05438a301
Jakub Smolarczyk <jakubjaceksmolarczyk@gmail.com>
2023-11-13 00:07:37 +0100

    split Notebook class(2)

--- Zajecia 3 - interfejs graficzny z uzyciem SWING/untitled2/SWING_zadanie2/src/Notebook.java
+++ Zajecia 3 - interfejs graficzny z uzyciem SWING/untitled2/SWING_zadanie2/src/Notebook.java
@@ -1,14 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
 
 public class Notebook {
-    JFrame frame;
-    JTextField field;
-    JTextArea area; //it works
-    JButton NEW, OPEN, SAVE, CLOSE;
+    private static JFrame frame;
+    private static JTextField field;
+    private static JTextArea area;
+    Buttons NEW, OPEN, SAVE, CLOSE;
     public void start(){
         init_frame();
         create_area();
@@ -19,6 +16,12 @@ public class Notebook {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+    public static JTextField getField(){
+        return field;
+    }
+    public static JTextArea getArea(){
+        return area;
     }
     public void init_frame(){
         frame = new JFrame("Notebook");
@@ -37,68 +40,17 @@ public class Notebook {
         area.setBorder(BorderFactory.createLineBorder(Color.black));
     }
     public void create_buttons(){
-        NEW = new JButton("NEW");
-        OPEN = new JButton("OPEN");
-        SAVE = new JButton("SAVE");
-        CLOSE = new JButton("CLOSE");
-
-        Buttons(NEW,100);
-        Buttons(OPEN,330);
-        Buttons(SAVE,560);
-        Buttons(CLOSE,790);
+        NEW = new Buttons(new JButton("NEW"),100);
+        OPEN = new Buttons(new JButton("OPEN"),330);
+        SAVE = new Buttons(new JButton("SAVE"),560);
+        CLOSE = new Buttons(new JButton("CLOSE"),790);
     }
     public void add_elements(){
         frame.add(area);
-        frame.add(NEW);
-        frame.add(OPEN);
-        frame.add(SAVE);
-        frame.add(CLOSE);
         frame.add(field);
-    }
-    public void Buttons(JButton button, int x){
-        button.setBounds(x,25,110,50);
-        String function = button.getText();
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switch(function){
-                    case "NEW":     NEW(); break;
-                    case "OPEN":    OPEN(); break;
-                    case "SAVE":    try {SAVE();} catch (IOException ex) {field.setText("An error occurred!"); throw new RuntimeException(ex);} break;
-                    case "CLOSE":   CLOSE(); break;
-                    default: System.out.println("oops"); break;
-                }
-            }
-        });
-    }
-
-    public void NEW(){
-        area.setText("");
-    }
-    public void OPEN(){
-        area.setText("");
-        String file_name = field.getText();
-        try {
-            FileReader input = new FileReader(file_name+".txt");
-            BufferedReader buffer = new BufferedReader(input);
-            String line;
-            while((line = buffer.readLine()) != null){
-                area.append(line+"\n");
-            }
-        } catch (FileNotFoundException e) {
-            field.setText("The file couldn't be found...");
-        } catch (IOException e) {
-            field.setText("Error in reading a file...");
-        }
-    }
-    public void SAVE() throws IOException {
-        String file_name = field.getText();
-        FileWriter output = new FileWriter(file_name+".txt");
-        output.write(area.getText());
-        output.close();
-        field.setText("The .txt file "+file_name+" is saved");
-    }
-    public void CLOSE(){
-        System.exit(2);
+        frame.add(NEW.getButton());
+        frame.add(OPEN.getButton());
+        frame.add(SAVE.getButton());
+        frame.add(CLOSE.getButton());
     }
 }
