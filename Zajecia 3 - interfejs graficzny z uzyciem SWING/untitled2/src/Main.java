5eaab7200caa7c469c64d2ac064583f010388ff6
maxster256 <132581406+maxster256@users.noreply.github.com>
2023-11-13 16:45:23 +0100

    Update Main.java

--- Zajecia 3 - interfejs graficzny z uzyciem SWING/untitled2/src/Main.java
+++ Zajecia 3 - interfejs graficzny z uzyciem SWING/untitled2/src/Main.java
@@ -1,21 +1,9 @@
-import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new Main().start();
+                new App().start();
             }
         });
-    }
-
-    public void start(){
-        JFrame frame = new JFrame("FirstSwingApp");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JLabel label = new JLabel("Hi!");
-        frame.getContentPane().add(label);
-
-        frame.pack();
-        frame.setVisible(true);
     }
 }
