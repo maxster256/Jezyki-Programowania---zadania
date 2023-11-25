ee2793731e14cefc980575911a8cbc03278327b7
Jakub Smolarczyk <jakubjaceksmolarczyk@gmail.com>
2023-11-25 18:16:14 +0100

    Mouse Reactions

--- Zajecia 4 - Obsluga zdarzen od myszy i klawiatury/Lab4_zadanie1/src/App.java
+++ Zajecia 4 - Obsluga zdarzen od myszy i klawiatury/Lab4_zadanie1/src/App.java
@@ -4,11 +4,9 @@ import java.awt.event.*;
 
 public class App extends JFrame {
     JPanel panel;
-    JButton button;
     public void start(){
         init_frame();
         create_panel();
-        create_button();
         add_elements();
 
         pack();
@@ -30,43 +28,9 @@ public class App extends JFrame {
                 System.out.println("Panel clicked at: " + e.getX() + ", " + e.getY());
                 panel.setBackground(Color.red);
             }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                System.out.println("press");
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                System.out.println("released");
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                System.out.println("entered");
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                System.out.println("exited");
-            }
-        });
-
-    }
-    public void create_button(){
-        button = new JButton("Click me!");
-        button.setBounds(350,325,100,50);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                remove(button);
-                validate();
-                repaint();
-            }
         });
     }
     public void add_elements(){
-        //add(button);
         add(panel);
     }
 }
